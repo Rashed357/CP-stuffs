b1b8ef716b0be046f5bbc5aab0219ff2df675e15
Rashed357 <118917961+Rashed357@users.noreply.github.com>
2024-10-03 11:01:09 +0600

    Create knapsack.cpp

---
+++ knapsack.cpp
@@ -0,0 +1,17 @@
+// Application: Solves the 0/1 Knapsack problem where we maximize value without exceeding weight capacity.
+int knapsack(vector<int>& weights, vector<int>& values, int W) {
+    int n = weights.size();
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0)); // DP table
+
+    for (int i = 1; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (weights[i - 1] <= w) {
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
+            } else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][W]; // Max value that can be achieved
+}
