ac699cf4a76ff9fdadce96aa341967fb79f8a6aa
Rashed357 <118917961+Rashed357@users.noreply.github.com>
2025-05-21 08:29:20 +0600

    Update number_of_divisors.cpp

--- number_of_divisors.cpp
+++ number_of_divisors.cpp
@@ -11,3 +11,24 @@ int number_of_divisors(int n) {
     }
     return res;
 }
+
+//from 1 to n.
+#include<bits/stdc++.h>
+using namespace std;
+
+int d[104];
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n = 100;
+  for (int i = 1; i <= n; i++) {
+    for (int j = i; j <= n; j += i) {
+      d[j]++;
+      // d[j] += i // for sum of divisors
+    }
+  }
+  for (int i = 1; i <= n; i++) {
+    cout << d[i] << ' ';
+  }
+  return 0;
+}
