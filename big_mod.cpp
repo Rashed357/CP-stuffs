fd2b6faa91b245d548c99380abb45e708778b8f0
Rashed357 <118917961+Rashed357@users.noreply.github.com>
2025-06-25 15:55:17 +0600

    Update and rename modExp.cpp to big_mod.cpp

---
+++ big_mod.cpp
@@ -0,0 +1,12 @@
+int big_mod(int a, int b) {
+    int res = 1;
+    a = a % mod;
+    while (b > 0) {
+        if (b % 2 == 1) {
+            res = (res * a) % mod;
+        }
+        a = (a * a) % mod;
+        b /= 2;
+    }
+    return res;
+}
