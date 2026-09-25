9afc61a5845bbe8dc076909ce046b6158650cbe0
Rashed357 <118917961+Rashed357@users.noreply.github.com>
2026-09-25 09:52:54 +0600

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -15,3 +15,61 @@ int knapsack(vector<int>& weights, vector<int>& values, int W) {
 
     return dp[n][W]; // Max value that can be achieved
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+const int N = 1005;
+const int W = 10005;
+
+int n, capacity;
+int wt[N], val[N];
+int dp[N][W];
+
+int solve(int i, int rem)
+{
+    // Base case
+    if(i == n)
+        return 0;
+
+    // Already calculated
+    if(dp[i][rem] != -1)
+        return dp[i][rem];
+
+    // Don't take current item
+    int ans = solve(i + 1, rem);
+
+    // Take current item
+    if(wt[i] <= rem)
+    {
+        ans = max(ans,
+                  val[i] + solve(i + 1, rem - wt[i]));
+    }
+
+    return dp[i][rem] = ans;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> capacity;
+
+    for(int i = 0; i < n; i++)
+        cin >> wt[i] >> val[i];
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << solve(0, capacity) << '\n';
+
+    return 0;
+}
+
+vector<int> dp(W + 1);
+for(int i = 0; i < n; i++)
+    for(int w = W; w >= wt[i]; w--)
+        dp[w] = max(dp[w], val[i] + dp[w - wt[i]]);
