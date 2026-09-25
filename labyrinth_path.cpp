71d68aa426ea1a45f0c472cee8d05ecbcf6dbdec
Rashed357 <118917961+Rashed357@users.noreply.github.com>
2026-09-25 09:55:40 +0600

    Refactor labyrinth pathfinding code structure

--- labyrinth_path.cpp
+++ labyrinth_path.cpp
@@ -1,119 +1,173 @@
+```cpp
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define ll long long
- 
-const int mx = 1007;
-bool vis[mx][mx];
-int dist[mx][mx];
-pair<int,int> parent[mx][mx];
-vector<string>v(mx);
-int n,m;
-pair<int,int> loc_a, loc_b;
- 
-int dx[] = {1,-1,0,0};
-int dy[] = {0,0,1,-1};
-char path[] = {'D','U','R','L'};
- 
-bool valid(int x, int y){
- 
-    return (x>=0 && x<n && y>=0 && y<m && dist[x][y]==-1 && v[x][y]!='#');
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+const int N = 1007;
+
+int n, m;
+
+// dist[x][y] = shortest distance from A to (x, y)
+// -1 means not visited
+int dist[N][N];
+
+// parent[x][y] = previous cell used to reach (x, y)
+pair<int, int> parent[N][N];
+
+vector<string> grid;
+
+// Positions of A and B
+pair<int, int> A, B;
+
+// Down, Up, Right, Left
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+// Character corresponding to each direction
+char moveChar[] = {'D', 'U', 'R', 'L'};
+
+
+// ------------------------------------------------------------
+// Check whether (x, y) is a valid cell to visit
+// ------------------------------------------------------------
+bool valid(int x, int y)
+{
+    return x >= 0 && x < n &&
+           y >= 0 && y < m &&
+           grid[x][y] != '#' &&
+           dist[x][y] == -1;
 }
- 
- 
- 
-void solve() {
- 
-    cin>>n>>m;
- 
-    for(int i=0; i<n; i++)
-        cin>>v[i];
- 
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(v[i][j]=='A'){
-                loc_a.first = i;
-                loc_a.second = j;
-            }
- 
-            if(v[i][j] == 'B'){
-                loc_b.first = i;
-                loc_b.second = j;
-            }
-        }
-    }
- 
-    memset(dist,-1,sizeof(dist));
-    dist[loc_a.first][loc_a.second] = 0;
-    parent[loc_a.first][loc_a.second] = loc_a;
-    
-    priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
- 
-    pq.push({0,loc_a});
- 
-    while(!pq.empty()){
- 
-        auto it = pq.top();
-        pq.pop();
- 
-        int cur_dist = it.first;
-        int x = it.second.first;
-        int y = it.second.second;
- 
-        if(x==loc_b.first && y==loc_b.second)
+
+
+// ------------------------------------------------------------
+// BFS: find shortest path from A to B
+// ------------------------------------------------------------
+void bfs()
+{
+    queue<pair<int, int>> q;
+
+    // Start from A
+    dist[A.first][A.second] = 0;
+    parent[A.first][A.second] = A;
+
+    q.push(A);
+
+    while(!q.empty())
+    {
+        auto [x, y] = q.front();
+        q.pop();
+
+        // We can stop once B is reached
+        if(make_pair(x, y) == B)
             break;
- 
-        for(int i=0; i<4; i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
- 
-            if(valid(nx,ny)){
-                dist[nx][ny] = cur_dist+1;
-                parent[nx][ny] = {x,y};
-                pq.push({cur_dist+1,{nx,ny}});
+
+        for(int dir = 0; dir < 4; dir++)
+        {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if(valid(nx, ny))
+            {
+                // Distance of neighbor
+                dist[nx][ny] = dist[x][y] + 1;
+
+                // Remember where we came from
+                parent[nx][ny] = {x, y};
+
+                q.push({nx, ny});
             }
         }
     }
- 
-    if(dist[loc_b.first][loc_b.second]==-1){
-        cout<<"NO"<<endl;
-        return;
-    }
- 
-    cout<<"YES"<<endl;
-    cout<<dist[loc_b.first][loc_b.second]<<endl;
- 
-    string way = "";
- 
-    pair<int,int> cur = loc_b;
- 
-    while(cur!=loc_a){
- 
-        pair<int,int>prev = parent[cur.first][cur.second];
- 
-        for(int i=0; i<4; i++){
-            if(prev.first+dx[i]==cur.first && prev.second+dy[i]==cur.second){
-                way += path[i];
+}
+
+
+// ------------------------------------------------------------
+// Reconstruct path from B -> A using parent[]
+// Then reverse it to get A -> B
+// ------------------------------------------------------------
+string getPath()
+{
+    string path;
+
+    pair<int, int> cur = B;
+
+    while(cur != A)
+    {
+        auto prev = parent[cur.first][cur.second];
+
+        // Find which direction takes prev -> cur
+        for(int dir = 0; dir < 4; dir++)
+        {
+            if(prev.first + dx[dir] == cur.first &&
+               prev.second + dy[dir] == cur.second)
+            {
+                path += moveChar[dir];
                 break;
             }
         }
- 
+
         cur = prev;
     }
- 
-    reverse(way.begin(),way.end());
- 
-    cout<<way<<endl;
- 
-    
+
+    reverse(path.begin(), path.end());
+
+    return path;
 }
- 
-int main() {
+
+
+// ------------------------------------------------------------
+// Solve
+// ------------------------------------------------------------
+void solve()
+{
+    cin >> n >> m;
+
+    grid.resize(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> grid[i];
+
+        for(int j = 0; j < m; j++)
+        {
+            if(grid[i][j] == 'A')
+                A = {i, j};
+
+            if(grid[i][j] == 'B')
+                B = {i, j};
+        }
+    }
+
+    // Initially, nobody is visited
+    memset(dist, -1, sizeof(dist));
+
+    // Find shortest path
+    bfs();
+
+    // B was never reached
+    if(dist[B.first][B.second] == -1)
+    {
+        cout << "NO\n";
+        return;
+    }
+
+    // B is reachable
+    cout << "YES\n";
+    cout << dist[B.first][B.second] << '\n';
+
+    string path = getPath();
+
+    cout << path << '\n';
+}
+
+
+int main()
+{
     fastio;
- 
+
     solve();
- 
+
     return 0;
 }
+```
